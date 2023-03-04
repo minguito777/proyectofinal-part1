f97cb4e580cd6972a77e5d9e2dde18fa4e89aba0
Domingo Morelli <mingomorelli67@gmail.com>
2023-03-04 03:19:53 -0300

    img

--- carpeta/data/stock.js
+++ carpeta/data/stock.js
@@ -32,7 +32,7 @@ const juegos = [
         tipo: "juego de playstation 2",
         desc: "Juego de mundo abierto",
         precio: 2500,
-        imagen: "/carpeta/img/gtaSA.jpg",
+        imagen: "carpeta/img/gtaSA.jpg",
         cantidad: 1
     },
     {
@@ -41,7 +41,7 @@ const juegos = [
         tipo: "juego de Nintendo WiiU",
         desc: "Juego de carreras",
         precio: 2800,
-        imagen: "/carpeta/img/marioKart8.jpg",
+        imagen: "carpeta/img/marioKart8.jpg",
         cantidad: 1
     },
     {
@@ -50,7 +50,7 @@ const juegos = [
         tipo: "juego de xbox 360",
         desc: "Juego de disparos en 1ra persona",
         precio: 3500,
-        imagen: "/carpeta/img/haloReach.jpg",
+        imagen: "carpeta/img/haloReach.jpg",
         cantidad: 1
     },
     {
@@ -59,7 +59,7 @@ const juegos = [
         tipo: "juego de playstation 2",
         desc: "Juego de sigilo",
         precio: 2350,
-        imagen: "/carpeta/img/hitman.jpg",
+        imagen: "carpeta/img/hitman.jpg",
         cantidad: 1
     },
     {
@@ -68,7 +68,7 @@ const juegos = [
         tipo: "juego de playstation 3",
         desc: "Juego de exploracion y mazmorras",
         precio: 2500,
-        imagen: "/carpeta/img/demonSouls.jpg",
+        imagen: "carpeta/img/demonSouls.jpg",
         cantidad: 1
     },
     {
@@ -77,7 +77,7 @@ const juegos = [
         tipo: "juego de Sega megadrive",
         desc: "Juego de peleas",
         precio: 4500,
-        imagen: "/carpeta/img/mortalKombat2.jpg",
+        imagen: "carpeta/img/mortalKombat2.jpg",
         cantidad: 1
     },
     {
@@ -86,7 +86,7 @@ const juegos = [
         tipo: "juego de Nintendo Gameboy",
         desc: "Juego de exploracion y combate por turnos",
         precio: 2500,
-        imagen: "/carpeta/img/pokemonRed.jpg",
+        imagen: "carpeta/img/pokemonRed.jpg",
         cantidad: 1
     },
     {
@@ -95,7 +95,7 @@ const juegos = [
         tipo: "juego de playstation 1",
         desc: "Juego survival horror",
         precio: 2820,
-        imagen: "/carpeta/img/residentEvil2.jpg",
+        imagen: "carpeta/img/residentEvil2.jpg",
         cantidad: 1
     },
     {
@@ -104,7 +104,7 @@ const juegos = [
         tipo: "juego de playstation 1",
         desc: "Juego de survival horror",
         precio: 3700,
-        imagen: "/carpeta/img/silentHill.png",
+        imagen: "carpeta/img/silentHill.png",
         cantidad: 1
     },
 ]
